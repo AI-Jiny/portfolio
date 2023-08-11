c885b66019825acd910a2177e245285de3bd3cc4
AI-Jiny <key140@naver.com>
2023-08-11 21:32:48 +0900

    demo html apply

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from myport.views import Main
+from django.conf.urls.static import static
+from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',Main.as_view()),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
--- myport/views.py
+++ myport/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
 
 # Create your views here.
+
+class Main(APIView):
+    def get(self, request):
+        return render(request, "index.html")
